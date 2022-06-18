84ebcbd2b35517f5567612ad0f1277280d05b329
MilorES <milorcete@gmail.com>
2022-06-18 09:33:25 +0200

    Se cambia el separador por un flecha cuando se muestra

--- m_diccionario.py
+++ m_diccionario.py
@@ -2,20 +2,21 @@
 #Alta
 def diccionario_alta (clave, valor, diccionario):
     diccionario[clave] = valor
-    return diccionario
+    #return diccionario
 
 # Baja                                            
 def diccionario_baja(clave, diccionario):
     del diccionario[clave]
-    return diccionario
+    #return diccionario
 
 # Actualizar
 def diccionario_actualizar(clave, valor, diccionario):
-    return diccionario_alta(clave, valor, diccionario)
+    diccionario_alta(clave, valor, diccionario)
+    #return diccionario_alta(clave, valor, diccionario)
 
 # Recorrer "Mostrando por pantalla"
 def diccionario_recorrer(diccionario):
     for item in diccionario.items():
-        print(item[0],item[1], sep=':')
+        print(item[0],item[1], sep=' → ')
 
 # Crear Modulo de diccionario que pueda Alta, Baja, Actualizar y Recorrer 
