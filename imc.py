b928eccdc412bf6dd8afdb55842b012338b0badd
Milor <milorcete@gmail.com>
2022-07-01 13:35:06 +0200

    Calculo del IMC y mostrar información por pantalla

---
+++ imc.py
@@ -0,0 +1,20 @@
+'''
+imc = peso / altura*altura
+Underweight imc < 18.5
+Normal imc > 18.5 %% 25 < imc
+Overweight imc > 25 a imc < 30
+Obesity imc > 30
+'''
+peso = float(input())
+altura = float(input())
+#imc = peso / (altura * altura)
+imc = peso / altura**2
+print(imc)
+if imc < 18.5:
+    print("Underweight")
+elif imc >= 18.5 and imc < 25:
+    print("normal")
+elif imc >= 25 and imc < 30:
+    print("Overweight")
+else:
+    print("Obesity")
