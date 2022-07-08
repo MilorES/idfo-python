8ab2a9bc4c9f492e4c207d18ed096e823c67c25d
MilorES <milorcete@gmail.com>
2022-07-08 17:27:12 +0200

    Empezando con binarios

---
+++ binarios.py
@@ -0,0 +1,27 @@
+import pickle
+from tkinter.messagebox import NO
+
+def guardar_usuarios(nombre_fichero:str,l_usuarios:list):
+    
+    fichero = open(nombre_fichero,'wb')
+    pickle.dump(l_usuarios, fichero)
+    fichero.close()
+
+def obtener_usuarios(nombre_fichero:str) -> list:
+    l_usuarios = None
+    try:
+        with open(nombre_fichero, 'rb') as f:
+            l_usuarios = pickle.load(f)   
+    except FileNotFoundError as exc:
+        print(exc)
+    except Exception as exc:
+        print(f"Error I/O {exc}")
+    return l_usuarios
+
+if __name__ == "__main__":
+    s_nombre_fichero = "usuarios.bin"
+    usuarios = ['matias','juana','marcos']
+    guardar_usuarios(s_nombre_fichero, usuarios)
+    usuarios = obtener_usuarios(s_nombre_fichero)
+    if(usuarios is not None):
+        print(usuarios)
